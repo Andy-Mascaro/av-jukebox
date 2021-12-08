776c7b1f02e683635974a10b3354ff86af8ce346
Andy Mascaro <amascaro1@hotmail.com>
2021-12-08 14:36:50 -0800

    test

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const audio = document.getElementById('audio');
 
-const ninjaSound = document.getElementById('car');
+const carSound = document.getElementById('car');
 carSound.addEventListener('click', () => {
-    audio.src = 'assets/sounds/car.mp3';
-    audio.play(); 
-
-const princessSound = document.getElementById('princess-sound');
-princessSound.addEventListener('click', () => {
-  audio.src = 'assets/sounds/princess.mp3';
+  audio.src = 'assets/sounds/car.mp3';
+  audio.play(); 
+});
+const rocketSound = document.getElementById('rocket-sound');
+rocketSound.addEventListener('click', () => {
+  audio.src = 'assets/sounds/rocket.mp3';
   audio.play();
 });
 
-const rangerSound = document.getElementById('ranger-sound');
-rangerSound.addEventListener('click', () => {
-  audio.src = 'assets/sounds/ranger.mp3';
+const boatSound = document.getElementById('boat-sound');
+boatSound.addEventListener('click', () => {
+  audio.src = 'assets/sounds/boat.mp3';
   audio.play();
 });
---
+++ data.js
@@ -0,0 +1,16 @@
+export const characters = [{
+    name: 'car',
+    image: 'assets/avatars/car.png',
+    audio: 'assets/sounds/car.mp3',
+}, {
+    name: 'rocket',
+    image: 'assets/avatars/rocket.png',
+    audio: 'assets/sounds/rocket.mp3'
+}, { name: 'boat',
+    image: 'assets/avatars/boat.png',
+    audio: 'assets/sounds/boat.mp3'
+}, 
+import { characters } from './data.js';
+
+console.log(characters);
+}];
